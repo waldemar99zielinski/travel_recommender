b93cdb4a7cc0ee34626e2313326de7af910121b0
Waldemar Zieliński <waldemar99zielinski@gmail.com>
2026-02-15 22:38:56 +0100

    ref(backend): aling with new models naming

--- backend/src/recommender/agents/preference_extraction/preference_extraction_agent.py
+++ backend/src/recommender/agents/preference_extraction/preference_extraction_agent.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from recommender.agents.base.base_agent import BaseAgent, BaseAgentBuilder
-from recommender.models.data_flow.user_preferences import UserPreferences
+from recommender.models.data_flow.user_preferences import UserInterestPreferences
 from recommender.models.llm.llm import create_llm_chat_model
 from recommender.models.llm.llm_config import LLMConfig
 from recommender.agents.preference_extraction.preference_extraction_prompt import prompt_tempalate
@@ -26,7 +26,7 @@ class PreferenceExtractionAgentBuilder(BaseAgentBuilder):
             self._prompt = prompt_tempalate
 
         if not self._output_type:
-            self._output_type = UserPreferences
+            self._output_type = UserInterestPreferences
 
         return PreferenceExtractionAgent(
             llm=llm,
@@ -40,7 +40,7 @@ class PreferenceExtractionAgent(BaseAgent):
     Uses a structured output Pydantic model for UserPreferenceEvaluation.
     """
 
-    def invoke(self, user_query: str) -> UserPreferences:
+    def invoke(self, user_query: str) -> UserInterestPreferences:
         input = self.prompt.format_messages(raw_user_query=user_query)
         return super().invoke(input)
 
--- backend/src/recommender/agents/preference_extraction/preference_extraction_prompt.py
+++ backend/src/recommender/agents/preference_extraction/preference_extraction_prompt.py
@@ -11,6 +11,7 @@ prompt_tempalate = ChatPromptTemplate.from_messages([
         - Only fill categories stated or implied by the user.
         - Attach categories not only on exact match but also on semtic meaning.
         - Extract all categories mentioned, even if only implied.
+        - Extract only interest categories from the schema.
         - If explicitly disliked, set strength=0 (do not omit it).
         - If not mentioned, keep it null.
         - extracted_text must be a short snippet from the user messages supporting the preference.
